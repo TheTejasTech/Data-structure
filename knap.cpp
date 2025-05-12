4439799e1e21effeb04ffbe3379fdd2e5bd50bd9
Tejas Kamble <kambletc@gmail.com>
2025-05-12 12:08:28 +0530

    Update knap.cpp

--- knap.cpp
+++ knap.cpp
@@ -5,20 +5,29 @@ const int N = 1e5 + 5;
 int val[105], wt[105];
 
 int main() {
-    int n, W;
-    cin >> n;
-    for (int i = 0; i < n; ++i) cin >> wt[i];
-    for (int i = 0; i < n; ++i) cin >> val[i];
-    cin >> W;
+    int n = 4;  // Number of items
+    int W = 5;  // Knapsack capacity
+
+    // Predefined weights and values of the items
+    int weights[] = {2, 3, 4, 5};
+    int values[] = {3, 4, 5, 6};
+
+    // Copy predefined weights and values into the arrays
+    for (int i = 0; i < n; ++i) {
+        wt[i] = weights[i];
+        val[i] = values[i];
+    }
 
     vector<int> dp(W + 1, 0);
 
+    // Dynamic Programming to solve 0/1 Knapsack
     for (int i = 0; i < n; ++i) {
         for (int w = W; w >= wt[i]; --w) {
             dp[w] = max(dp[w], dp[w - wt[i]] + val[i]);
         }
     }
 
+    // Output the maximum value that can be obtained
     cout << dp[W] << endl;
     return 0;
 }
